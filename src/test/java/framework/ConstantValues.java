cef117e3bb704bf0ff754d00cbbd029d651d1cfa
Bahtir <albahtir@gmail.com>
2025-06-24 05:11:32 -0400

    upgraded JUNIT to v 5.8.1

--- src/test/java/framework/ConstantValues.java
+++ src/test/java/framework/ConstantValues.java
@@ -21,6 +21,14 @@ public class ConstantValues {
     public static final String ID_LOGIN_BUTTON = "#login-button";
     public static final String ERROR_LOCKEDOUT_USER = "h3";
 
+    //Main Page ID's
+    public static final String SHOPPING_CART = "#shopping_cart_container";
+    public static final String BACKPACK = "#item_4_title_link";
+    public static final String BIKE_LIGHT = "#item_0_title_link";
+    public static final String BOLT_T_SHIRT = "#item_1_title_link";
+    public static final String FLEECE_JACKET = "#item_5_title_link";
+    public static final String ONESIE = "#item_2_title_link";
+    public static final String RED_T_SHIRT = "#item_3:_title_link";
 
     //Accessible ID's on sidebar Menu
     public static final String MENU_BUTTON = "#react-burger-menu-btn";
--- src/test/java/saucedemoTests/LoginTest.java
+++ src/test/java/saucedemoTests/LoginTest.java
@@ -1,17 +1,36 @@
 package saucedemoTests;
 
+import com.codeborne.selenide.WebDriverRunner;
 import framework.BaseClass;
-import org.junit.Test;
-
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static framework.ConstantValues.*;
 
 public class LoginTest extends BaseClass {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupTest() {
+         driver = new ChromeDriver();
+         ChromeOptions options = new ChromeOptions();
+         WebDriverRunner.setWebDriver(driver); // bind to Selenide
+        
+         // Disable Chrome credential service & safety check
+         options.addArguments("--disable-features=SafetyCheck");
+         options.addArguments("--disable-features=PasswordCheck");
+
+    }
 
     @Test
     public void loginTest(){
+
         open("https://www.saucedemo.com/");
 
         // Check that login page is the initial page.
@@ -33,4 +52,10 @@ public class LoginTest extends BaseClass {
 
 
     }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.close();
+    }
+
 }
